c0e863e725245216c8641058927cd4ee9212b352
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-07-13 15:19:07 +0300

    up

--- TheWatcher/urls.py
+++ TheWatcher/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Bot.R2D2 import start
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('start/', start)
 ]
